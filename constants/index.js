07d19a9cce3a286b25a4d9e899554dd746d58f29
Mr-Dhanush-git <dhanushchinthalapudi@gmail.com>
2026-08-25 01:09:06 +0530

    feat: implement navbar role rotation

---
+++ constants/index.js
@@ -0,0 +1,20 @@
+const roles = [
+  "designer",
+  "developer",
+  "tech enthusiast",
+  "future pirate king 🏴‍☠️",
+];
+
+const navLinks = [
+  { name: "Home", href: "#home" },
+  { name: "Projects", href: "#projects" },
+  { name: "About", href: "#about" },
+  { name: "Contact me", href: "#contact" },
+];
+
+const resumeLink = {
+  name: "@ Resume",
+  href: "/resume.pdf",
+};
+
+export { roles, navLinks, resumeLink };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { navLinks, roles, resumeLink } from '../../constants'
 
 const NavBar = () => {
+  const [phase, setPhase] = useState("forward");
+  const [roleIndex, setRoleIndex] = useState(0);
+
+useEffect(() => {
+  let delay;
+
+  if (phase === "forward" || phase === "pauseAtEnd") {
+    delay = 3000;
+  } else if (phase === "reverse") {
+    delay = 1000 / (roles.length - 1);
+  } else if (phase === "pauseAtStart") {
+    delay = 3000;
+  }
+
+  const timer = setTimeout(() => {
+    if (phase === "forward") {
+      setRoleIndex((prev) => {
+        const next = prev + 1;
+
+        if (next === roles.length - 1) {
+          setPhase("pauseAtEnd");
+        }
+
+        return next;
+      });
+    }
+
+    else if (phase === "pauseAtEnd") {
+      setPhase("reverse");
+    }
+
+    else if (phase === "reverse") {
+      setRoleIndex((prev) => {
+        const next = prev - 1;
+
+        if (next === 0) {
+          setPhase("pauseAtStart");
+        }
+
+        return next;
+      });
+    }
+
+    else if (phase === "pauseAtStart") {
+      setPhase("forward");
+    }
+  }, delay);
+
+  return () => clearTimeout(timer);
+}, [phase, roleIndex]);
+
+
+
   return (
-    <div>
-      Nav
-    </div>
+    <nav>
+      <div>
+        <a href="#home" className=''>
+          <p>
+            Dhanush <span>• {roles[roleIndex]}</span>
+          </p>
+        </a>
+      </div>
+
+      <div>
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.href}>
+              <a href={`${link.href}`}>{link.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+        <a href={resumeLink.href}>
+          {resumeLink.name}
+        </a>
+
+    </nav>
   )
 }
 
